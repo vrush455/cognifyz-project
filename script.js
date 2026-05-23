f208f8f0e9db77c3ff6a9a2c1591f7a21b1b1a47
Vrushali Dhakane <vrushalidhakane185@gmail.com>
2026-05-23 18:05:52 +0530

    Task 6: Add form with the CSS styling and Javascript validation

--- script.js
+++ script.js
@@ -23,3 +23,42 @@ fetchBtn.addEventListener("click", function() {
       });
     });
 });
+
+let form = document.getElementById("myForm");
+let feedback = document.getElementById("form-feedback");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  if (name === "") {
+    feedback.style.color = "red";
+    feedback.textContent = "❌ Please enter your name!";
+    return;
+  }
+
+  if (email === "") {
+    feedback.style.color = "red";
+    feedback.textContent = "❌ Please enter your email!";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    feedback.style.color = "red";
+    feedback.textContent = "❌ Please enter a valid email!";
+    return;
+  }
+
+  if (message === "") {
+    feedback.style.color = "red";
+    feedback.textContent = "❌ Please enter a message!";
+    return;
+  }
+
+  feedback.style.color = "green";
+  feedback.textContent = "✅ Message sent successfully!";
+  form.reset();
+});
